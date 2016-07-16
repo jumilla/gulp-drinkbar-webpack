d4ee6ab2ad640a28eeae69f56222301687473992
Fumio Furukawa <fumio@jumilla.me>
2016-07-16 22:35:13 +0900

    First version.

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var stream = require('webpack-stream');
+
+stream.babelLoader = require('babel-loader');
+
+module.exports = stream;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+const stream = require('webpack-stream')
+
+stream.babelLoader = require('babel-loader')
+
+module.exports = stream
