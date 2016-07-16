1934d0cde7cb3a535685c1df402754222f5d6555
Fumio Furukawa <fumio.furukawa@straight-spirits.co.jp>
2016-07-17 00:22:45 +0900

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,102 @@
 'use strict';
 
-var stream = require('webpack-stream');
+var _through = require('through2');
 
-stream.babelLoader = require('babel-loader');
+var _through2 = _interopRequireDefault(_through);
 
-module.exports = stream;
+var _gulpUtil = require('gulp-util');
+
+var _gulpUtil2 = _interopRequireDefault(_gulpUtil);
+
+var _webpack = require('webpack');
+
+var _webpack2 = _interopRequireDefault(_webpack);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _objectAssign = require('object-assign');
+
+var _objectAssign2 = _interopRequireDefault(_objectAssign);
+
+var _memoryFs = require('memory-fs');
+
+var _memoryFs2 = _interopRequireDefault(_memoryFs);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var PLUGIN_NAME = 'gulp-drinkbar-webpack';
+
+var defaultWebpackOptions = {
+	entry: '',
+	output: {
+		filename: '[name].js'
+	},
+	plugins: []
+};
+
+function webpackStream() {
+	var options = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+
+	if (typeof options === 'string') {
+		options = { output: { filename: options } };
+	}
+
+	var destFile = void 0;
+	var entries = [];
+
+	var transform = function transform(file, encode, done) {
+		var stream = this;
+
+		if (file.isNull()) {
+			done();
+		} else if (file.isStream()) {
+			stream.emit('error', new _gulpUtil2.default.PluginError(PLUGIN_NAME, 'Streaming not supported'));
+			done();
+		} else {
+			if (!destFile) {
+				destFile = new _gulpUtil2.default.File({
+					cwd: file.cwd,
+					base: file.base,
+					path: _path2.default.join(file.base, options.output ? options.output.filename : 'bundle.js')
+				});
+			}
+			entries.push(file.path);
+			done();
+		}
+	};
+
+	var flush = function flush(done) {
+		var stream = this;
+		var webpackOptions = (0, _objectAssign2.default)({}, defaultWebpackOptions, { entry: entries }, options);
+
+		try {
+			(function () {
+				var compiler = (0, _webpack2.default)(webpackOptions);
+
+				var fs = compiler.outputFileSystem = new _memoryFs2.default();
+
+				compiler.run(function (error, stats) {
+					if (error) {
+						stream.emit('error', new _gulpUtil2.default.PluginError(PLUGIN_NAME, error.message));
+					} else {
+						destFile.contents = new Buffer(fs.readFileSync(_path2.default.join(compiler.outputPath, options.output.filename), 'utf8'));
+
+						stream.push(destFile);
+					}
+					done();
+				});
+			})();
+		} catch (error) {
+			stream.emit('error', new _gulpUtil2.default.PluginError(PLUGIN_NAME, error.message));
+			done();
+		}
+	};
+
+	return _through2.default.obj(transform, flush);
+}
+
+webpackStream.babelLoader = require('babel-loader');
+
+module.exports = webpackStream;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,90 @@
 
-const stream = require('webpack-stream')
+const PLUGIN_NAME = 'gulp-drinkbar-webpack'
 
-stream.babelLoader = require('babel-loader')
+import through from 'through2'
+import gutil from 'gulp-util'
+import webpack from 'webpack'
+import path from 'path'
+import assing from 'object-assign'
+import MemoryFileSystem from 'memory-fs'
 
-module.exports = stream
+
+
+const defaultWebpackOptions = {
+	entry: '',
+	output: {
+		filename: '[name].js',
+	},
+	plugins: [],
+}
+
+
+
+function webpackStream(options = {}) {
+	if (typeof options === 'string') {
+		options = {output: {filename: options}}
+	}
+
+	let destFile
+	const entries = []
+
+	const transform = function (file, encode, done) {
+		const stream = this
+
+		if (file.isNull()) {
+			done()
+		}
+		else if (file.isStream()) {
+			stream.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
+			done()
+		}
+		else {
+			if (!destFile) {
+				destFile = new gutil.File({
+					cwd: file.cwd,
+					base: file.base,
+					path: path.join(file.base, options.output ? options.output.filename : 'bundle.js')
+				})
+			}
+			entries.push(file.path)
+			done()
+		}
+	}
+
+	const flush = function (done) {
+		const stream = this
+		const webpackOptions = assing({}, defaultWebpackOptions, {entry: entries}, options)
+
+		try {
+			const compiler = webpack(webpackOptions)
+
+			const fs = compiler.outputFileSystem = new MemoryFileSystem()
+
+			compiler.run((error, stats) => {
+				if (error) {
+					stream.emit('error', new gutil.PluginError(PLUGIN_NAME, error.message));
+				}
+				else {
+					destFile.contents = new Buffer(fs.readFileSync(path.join(compiler.outputPath, options.output.filename), 'utf8'))
+
+					stream.push(destFile)
+				}
+				done()
+			})
+		}
+		catch (error) {
+			stream.emit('error', new gutil.PluginError(PLUGIN_NAME, error.message));
+			done()
+		}
+	}
+
+	return through.obj(transform, flush)
+}
+
+
+
+webpackStream.babelLoader = require('babel-loader')
+
+
+
+module.exports = webpackStream
